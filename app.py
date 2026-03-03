448314550b88ad30a3751674fe8c9ab657a90ce8
AnouarKO <aeo6@alumnes.udl.cat>
2026-03-03 10:30:56 +0100

    port 8080

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def index():
 	return render_template("index.html", grup=grup)
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=80, debug=False)
+	app.run(host="0.0.0.0", port=8080, debug=False)
